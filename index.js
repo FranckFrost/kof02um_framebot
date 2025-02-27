87f458ffccfde3132f23f01bf1d6f9e7baccf8ad
FranckFrost <101209171+FranckFrost@users.noreply.github.com>
2025-02-27 18:42:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 const keepAlive = require('./server');
 const path = require('path')
-// const fetch = require('node-fetch'); to fetch the cargo table (from the module node-fetch@2, pas node-fetch)
+const fetch = require('node-fetch'); // to fetch the cargo table (from the module node-fetch@2, pas node-fetch)
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -47,10 +47,21 @@ client.once('ready', () => {
 client.on('interactionCreate', async autocomplete => {
 	if (!autocomplete.isAutocomplete()) return;
   // console.log(autocomplete.commandName)
-	if (autocomplete.commandName === 'embed' || autocomplete.commandName === 'frames') {
+	if (autocomplete.commandName === 'embed' || autocomplete.commandName === 'frames' || autocomplete.commandName === 'cargo') {
     let currentOption = autocomplete.options.getFocused(true);
     let currentName = currentOption.name;
     let currentValue = currentOption.value;
+
+    if (autocomplete.commandName === 'cargo'){
+	    let cargo_characters = []
+	    const url_char = "https://dreamcancel.com/w/index.php?title=Special:CargoExport&tables=MoveData_KOF02UM%2C&&fields=MoveData_KOF02UM.chara%2C&&group+by=MoveData_KOF02UM.chara&order+by=&limit=100&format=json"
+	    const response_char = await fetch(url_char);
+	    const cargo_char = await response_char.json();
+	    for (let x in cargo_char) {
+		    cargo_characters.push(cargo_char[x]["chara"])
+	    }
+	    characters = cargo_characters;
+    }
 
     const options = [];
     if (currentName === "character") {
@@ -71,70 +82,67 @@ client.on('interactionCreate', async autocomplete => {
     if (currentName === "move" && character !== "") {
       // currentValue = autocomplete.options.getFocused()
       let moveObj = {}
-      if (json[character] === undefined) {
-        // Capitilize first letter of char name.
-        let char = character.charAt(0).toUpperCase() + character.slice(1);
-        // Temp: validate extra names.
-        if (char === 'Mary') {
-          char = 'Blue Mary'
-            }
-        if (char === 'O.Chris') {
-          char = 'Orochi Chris'
-            }
-        if (char === 'O.Shermie') {
-          char = 'Orochi Shermie'
-            }
-        if (char === 'O.Yashiro') {
-          char = 'Orochi Yashiro'
-            }
-        if (char === 'Ex kensou' ||
-            char === 'Ex Kensou') {
-          char = 'EX Kensou'
-            }
-        if (char === 'Ex robert' ||
-            char === 'Ex Robert') {
-          char = 'EX Robert'
-            }
-        if (char === 'Ex takuma' ||
-            char === 'Ex Takuma') {
-          char = 'EX Takuma'
-            }
-        if (char === 'K Dash' ||
-            char === 'K`') {
-          char = 'K'
-            }
-        if (char === 'May Lee' ||
-            char === 'May Lee(Standard)') {
-          char = 'May Lee(Normal)'
-            }
-        character = char
-      }
-      if (json[character] === undefined) {
-        moveObj["name"] = 'Moves not found for specified character, try another character';
-        moveObj["value"] = 'Moves not found for specified character, try another character';
-        options.push(moveObj);
-      } else {
-        let moves = [] 
-        Object.keys(json[character]).forEach(function (key) {
-          moves.push(key);
-        })
-        // console.log(moves)
-        // console.log('currval ' + currentValue)
-        moves.forEach((move) => {
-          if (move.toLowerCase().includes(currentValue.toLowerCase())) {
-            moveObj = {}
-            moveObj["name"] = move;
-            moveObj["value"] = move;
-            // console.log(move)
-            if (options.length < 25) {
-              options.push(moveObj);
-            }
-          }
-        }) 
-      }
+      let moves = [];
+	    if (autocomplete.commandName === 'cargo') {
+		    if (!cargo_characters.includes(character)) {
+			    moveObj["name"] = 'No cargo data available for specified character. Gather framedata with /frames instead.';
+                            moveObj["value"] = 'No cargo data available for specified character. Gather framedata with /frames instead.';
+                            options.push(moveObj);
+		    } else {
+			    let move = "";
+			    const url_moves = "https://dreamcancel.com/w/index.php?title=Special:CargoExport&tables=MoveData_KOF02UM%2C&&fields=MoveData_KOF02UM.input%2C+MoveData_KOF02UM.input2%2C+MoveData_KOF02UM.name%2C&where=chara+%3D+%22"+character+"%22&order+by=MoveData_KOF02UM._ID+ASC&limit=100&format=json"
+			    const response_moves = await fetch(url_moves);
+			    const cargo_moves = await response_moves.json();
+			    for (let x in cargo_moves) {
+				    move = cargo_moves[x]["name"] + "([" + cargo_moves[x]["input"] + "] / [" + cargo_moves[x]["input2"] + "])"
+				    moves.push(move)
+			    }
+			    moves.forEach((move) => {
+				    if (move.toLowerCase().includes(currentValue.toLowerCase())) {
+					    moveObj = {}
+					    moveObj["name"] = move;
+					    moveObj["value"] = move;
+					    if (options.length < 25) options.push(moveObj);
+				    }
+			    }
+					  }
+	    } else {
+		    // Capitilize first letter of char name.
+		    let char = character.charAt(0).toUpperCase() + character.slice(1);
+		    // Validate extra names.
+		    if (char === 'Mary') char = 'Blue Mary';
+		    if (char === 'O.Chris') char = 'Orochi Chris';
+		    if (char === 'O.Shermie') char = 'Orochi Shermie';
+		    if (char === 'O.Yashiro') char = 'Orochi Yashiro';
+		    if (char === 'Ex kensou' || char === 'Ex Kensou') char = 'EX Kensou';
+		    if (char === 'Ex robert' || char === 'Ex Robert') char = 'EX Robert';
+		    if (char === 'Ex takuma' || char === 'Ex Takuma') char = 'EX Takuma';
+		    if (char === 'K Dash' || char === 'K`') char = 'K';
+		    if (char === 'May Lee' || char === 'May Lee(Standard)') char = 'May Lee(Normal)'
+		    character = char
+		    if (json[character] === undefined) {
+			    moveObj["name"] = 'Moves not found for specified character, try another character';
+			    moveObj["value"] = 'Moves not found for specified character, try another character';
+			    options.push(moveObj);
+		    } else {
+			    Object.keys(json[character]).forEach(function (key) {
+				    moves.push(key);
+			    })
+			    // console.log(moves)
+			    // console.log('currval ' + currentValue)
+			    moves.forEach((move) => {
+				    if (move.toLowerCase().includes(currentValue.toLowerCase())) {
+					    moveObj = {}
+					    moveObj["name"] = move;
+					    moveObj["value"] = move;
+					    // console.log(move)
+					    if (options.length < 25) options.push(moveObj);
+				    }
+			    })
+					  }
+	    }
+	    await autocomplete.respond(options);
     }
-		await autocomplete.respond(options);
-	}
 });
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
